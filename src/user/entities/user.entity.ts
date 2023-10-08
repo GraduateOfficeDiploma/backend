7d4f89d5a10bf7347434e53ae9a0c52fc62a4e3a
Vadym <cn.klose5567@gmail.com>
2023-10-08 18:54:30 +0300

    fix: db connection

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: "user" })
+@Entity({ name: 'user' })
 export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
