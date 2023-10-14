30e4159c4f36fba7f82cb4fc21b12b85ed80f5ce
nerance <nerow.services@gmail.com>
2023-10-14 19:52:45 +0300

    feat: swagger installation

---
+++ src/swaggerConfig.ts
@@ -0,0 +1,13 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API documentation for your Nest.js application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+}
