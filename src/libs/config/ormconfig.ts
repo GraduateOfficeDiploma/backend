ca344d414480465d2cb98e6868319a9931703e37
nerance <nerow.services@gmail.com>
2023-10-17 19:45:36 +0300

    feat: added db connection for heroku

--- src/libs/config/ormconfig.ts
+++ src/libs/config/ormconfig.ts
@@ -1,18 +1,30 @@
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+const dbConfig = process.env.DATABASE_URL
+  ? {
+      url: process.env.DATABASE_URL,
+      type: 'postgres',
+      namingStrategy: new SnakeNamingStrategy(),
+      synchronize: true, // Set to true for development, but false in production
+      extra: {
+        ssl: {
+          rejectUnauthorized: false, // You can set this to true if using Heroku's provided SSL certificate
+        },
+      },
+    }
+  : {
+      type: 'postgres',
+      host: process.env.PGHOST || 'localhost',
+      port: +process.env.PGPORT || 5432,
+      username: process.env.PGUSER || 'admin',
+      password: process.env.PGPASSWORD || 'admin',
+      database: process.env.PGDATABASE || 'graduates_office',
+      namingStrategy: new SnakeNamingStrategy(),
+      synchronize: true, // Set to true for development, but false in production
+    };
+
 export const OrmConfigOptions: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: process.env.PGHOST,
-  port: +process.env.PGPORT,
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  ...dbConfig,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  namingStrategy: new SnakeNamingStrategy(),
-  synchronize: true,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false }
-      : false,
 };
